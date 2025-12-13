df7c7e52d12a5f93b58adb94e4f62a5a54d99475
Yuval-Zikri <yuval.zikri2211@gmail.com>
2025-12-13 16:27:19 +0200

    add K8S

--- backend/config.py
+++ backend/config.py
@@ -7,9 +7,9 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Mail
-    MAIL_SERVER = os.getenv('MAIL_SERVER', 'smtp.mailtrap.io')
-    MAIL_PORT = int(os.getenv('MAIL_PORT', 2525))
+    MAIL_SERVER = os.getenv('MAIL_SERVER', 'smtp.gmail.com')
+    MAIL_PORT = int(os.getenv('MAIL_PORT', 587))
     MAIL_USE_TLS = os.getenv('MAIL_USE_TLS', 'True') == 'True'
     MAIL_USERNAME = os.getenv('MAIL_USERNAME')
     MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
-    MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER', 'noreply@eventmanager.com')
+    MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER', 'noreply.eventnotify@gmail.com')
