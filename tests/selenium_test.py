31888bbfbed934cfc1b067b18481ad0f5340f6e9
Yuval Zikri <yuvalz@consist-sys.consist.co.il>
2025-12-27 16:02:58 +0200

    fix: use React-aware JS setter for date in edit modal to prevent invalid format

--- tests/selenium_test.py
+++ tests/selenium_test.py
@@ -395,12 +395,17 @@ def test_full_ui_flow():
         # Change the date to trigger RSVP reset
         print("Changing date to trigger RSVP reset...")
         date_edit = driver.find_element(By.NAME, "date")
-        # Set to next year instead of this year
-        new_date_val = "2026-01-01T20:00"
-        date_edit.send_keys(new_date_val)
-        driver.execute_script("arguments[0].dispatchEvent(new Event('input', { bubbles: true }));", date_edit)
-        driver.execute_script("arguments[0].dispatchEvent(new Event('change', { bubbles: true }));", date_edit)
-        time.sleep(0.5)
+        
+        # Use the same React-aware setter we used in Part 1
+        driver.execute_script("""
+            var input = arguments[0];
+            var nativeInputValueSetter = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, "value").set;
+            nativeInputValueSetter.call(input, '2025-12-31T20:00');
+            var event = new Event('input', { bubbles: true });
+            input.dispatchEvent(event);
+            input.dispatchEvent(new Event('change', { bubbles: true }));
+        """, date_edit)
+        time.sleep(1)
         
         # Save - use a very specific selector for the "Save Changes" button in the modal
         print("Looking for Save button in modal...")
